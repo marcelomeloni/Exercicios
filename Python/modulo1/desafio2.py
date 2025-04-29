57bdeb43055699155ba7d9baa4a3a86b65768a51
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-04-29 10:46:43 -0300

    Update desafio2.py

--- Python/modulo1/desafio2.py
+++ Python/modulo1/desafio2.py
@@ -1,1 +1,7 @@
+#receber a data de nascimento da pessoa e exibir de maneira formatada
 
+dia = input('Qual dia você nasceu?')
+mes = input('Qual mês você nasceu?')
+ano = input('Qual ano você nasceu?')
+
+print('Você nasceu no dia', dia, "de", mes, "de", ano)
