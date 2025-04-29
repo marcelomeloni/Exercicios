5f2ed862d7df5126f4baa61164a6474b5ce4cb81
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-04-29 11:48:02 -0300

    Create desafio5.py

---
+++ Python/modulo1/desafio5.py
@@ -0,0 +1,6 @@
+#faça um programa que mostre o sucessor e o antecessor do número obtido da entrada
+
+num = int(input('Digite um número: '))
+antecessor = num - 1
+sucessor = num + 1
+print("O antecessor de {} é {} \nO sucessor de {} é {}".format(num,antecessor,num,sucessor))
