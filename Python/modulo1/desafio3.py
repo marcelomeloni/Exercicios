a4819079ed97a8393b5dffc5c99e5c93bb60322a
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-04-29 10:52:42 -0300

    Create desafio3.py

---
+++ Python/modulo1/desafio3.py
@@ -0,0 +1,7 @@
+#receber dois numeros e efetuar a soma
+
+n1 = int(input('Qual o primeiro número?'))
+n2 = int(input('Qual o segundo número?'))
+
+soma = n1 + n2
+print("A soma de", n1, "+", n2, " é:", soma)
