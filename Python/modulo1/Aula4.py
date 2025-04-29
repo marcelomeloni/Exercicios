ad2cdd86d3df2a551a29dfd4d660bd824418d733
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-04-29 10:39:41 -0300

    Update Aula4.py

--- Python/modulo1/Aula4.py
+++ Python/modulo1/Aula4.py
@@ -1,1 +1,19 @@
+print('Olá Mundo!')
 
+print(7+4)
+
+print('7'+'4')
+
+print('Olá', 5)
+
+nome = 'Marcelo'
+idade = 19
+peso = 78
+
+print(nome,idade,peso)
+
+nome = input('Qual é o seu nome?')
+idade = input('Quantos anos você tem?')
+peso = input('Qual é o seu peso?')
+
+print(nome,idade,peso)
