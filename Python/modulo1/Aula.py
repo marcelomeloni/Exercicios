b3ffb52235b0f1bf4d7207e8f5ec17f7d0030c74
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-04-30 13:37:07 -0300

    Create Aula.py

---
+++ Python/modulo1/Aula.py
@@ -0,0 +1,38 @@
+# Aula 9 - Manipulando texto
+
+
+frase = 'Curso em Video python'
+
+print(frase[9:14])
+
+print(frase[9:21])
+
+print(frase[9:21:2])
+
+print(frase[:5])
+
+print(frase[9::3])
+
+print(len(frase))
+
+print(frase.count('o',0,14))
+
+print(frase.find('deo'))
+
+print('Curso' in frase)
+
+print(frase.upper())
+
+print(frase.lower())
+
+print(frase.capitalize())
+
+print(frase.title())
+
+frase = '   Aprenda Python '
+
+print(len(frase))
+
+print(frase.strip())
+
+print(len(frase))
