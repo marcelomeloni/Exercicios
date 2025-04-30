7dccf305b0dc39b308f8e3f628a519fee6703b76
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-04-30 13:03:41 -0300

    Create desafio18.py

---
+++ Python/modulo1/desafio18.py
@@ -0,0 +1,10 @@
+# Cosseno, seno e tangente de um anglo 
+
+import math
+angle = float(input('Digite o ânglo: '))
+
+cos = math.cos(angle)
+sin = math.sin(angle)
+tg = math.tan(angle)
+
+print('Cosseno: {}\nSeno: {}\nTangente: {}'.format(cos, sin, tg))
