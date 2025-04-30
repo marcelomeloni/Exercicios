93658c72564af04841bf5fdac17074b4e4b98341
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-04-30 12:07:58 -0300

    Create desafio8.py

---
+++ Python/modulo1/desafio8.py
@@ -0,0 +1,4 @@
+# Conversor de metros para centimetros e milimetros
+
+metro = int(input('Digite o valor da distancia em metros: \n'))
+print('A distancia em centimetros é {}cm, e em milimetros {}mm'.format(metro*100,metro*1000))
