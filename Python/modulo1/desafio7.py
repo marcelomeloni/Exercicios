19d0239be1e2465fa66d601918293fe9789edbf6
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-04-30 12:04:17 -0300

    Create desafio7.py

---
+++ Python/modulo1/desafio7.py
@@ -0,0 +1,8 @@
+# Desenvolva um programa que leia duas notas de um aluno. Calcule e mostre a media
+
+n1 = int(input('Digite a primeira nota: \n'))
+n2 = int(input('Digite a segunda nota: \n'))
+
+media = (n1+n2)/2
+
+print('A sua media do aluno é {}'.format(media))
