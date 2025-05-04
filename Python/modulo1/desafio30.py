7af4012b5ced8fdd6b0125fc634b1ab99874580a
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-05-04 13:10:46 -0300

    Update desafio30.py

--- Python/modulo1/desafio30.py
+++ Python/modulo1/desafio30.py
@@ -1,5 +1,14 @@
 #  Crie um programa que leia um número inteiro e mostre na tela se ele é PAR ou ÍMPAR.
-num = int(input("Digite um número para verificar se ele é primo: "))
+
+
+
+num = int(input("Digite um número para verificar se ele é primo, e se ele é par ou impar: "))
+
+if num % 2 == 0:
+    print("O número {} é par".format(num))
+else:
+    print("O número {} é impar".format(num))
+
 primo = True
 for x in range(2,num):
     if num % x == 0:
