8b4186b3f0fc9c38499521f487e600d0d79c4e99
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-05-05 12:56:05 -0300

    Create desafio47.py

---
+++ Python/modulo1/desafio47.py
@@ -0,0 +1,7 @@
+'''
+Exercício Python 47: Crie um programa que mostre na tela todos os números pares que estão no intervalo entre 1 e 50.
+'''
+
+for num in range(0,51):
+ if num % 2 == 0:
+     print(num)
