8cab129281834b4a9e8372012c68c47d402ce2e0
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-05-06 15:25:01 -0300

    Create desafio58.py

---
+++ Python/modulo1/desafio58.py
@@ -0,0 +1,10 @@
+'''
+
+Exercício Python 57: Faça um programa que leia o sexo de uma pessoa, 
+mas só aceite os valores ‘M’ ou ‘F’.
+Caso esteja errado, peça a digitação novamente até ter um valor correto.
+'''
+sexo = str(input('Digite seu sexo: [M/F]')).strip().upper()[0]
+while sexo not in 'MF':
+    sexo = str(input('Dado invalido. Por favor informe seu sexo. [M/F]')).strip().upper()[0]
+print('Sexo {} registrado com sucesso!'.format(sexo))
