080804756753efd9f44a24e83c8f0b4c26998dd1
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-08-07 09:08:26 -0300

    Create exercicio2.py

---
+++ Python/Asimov Academy/exercicio2.py
@@ -0,0 +1,9 @@
+# Exercício de Python 2: média de uma lista
+numeros = [10, 20, 30, 40, 50]
+soma_media = 0 
+for n in range(0,len(numeros)):
+        soma_media += numeros[n]
+media = soma_media/5
+print(media)
+
+# Saida: 30.0
