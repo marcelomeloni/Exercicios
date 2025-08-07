5a629727e8e912496835d160f90913bd701e15ef
Marcelo Meloni <132206380+marcelomeloni@users.noreply.github.com>
2025-08-07 09:08:07 -0300

    Create exercicio1.py

---
+++ Python/Asimov Academy/exercicio1.py
@@ -0,0 +1,9 @@
+# Exercício de Python 1: apresente-se!
+
+nome = "Ana"
+idade = 25
+cidade = "São Paulo"
+
+print(f'Olá. Meu nome é {nome}. Eu tenho {idade} anos e moro em {cidade}')
+
+# Saida: Olá. Meu nome é Ana. Eu tenho 25 anos e moro em São Paulo
